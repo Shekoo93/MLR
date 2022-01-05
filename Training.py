6c6b7b986c3210fffe594deee77ad4c3f8bbc296
Shekoofeh <78698281+Shekoo93@users.noreply.github.com>
2022-01-05 12:41:50 -0500

    Update Training.py

--- Training.py
+++ Training.py
@@ -1,35 +1,28 @@
-#Code to train the MLR model's visual knowledge  (a modified variational autoencoder)
+#This code train the mVAE part of the MLR model
 
 
-
-# prerequisites
+#prerequisites
 import torch
 import numpy as np
 from sklearn import svm
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
-
-
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import torch.optim as optim
 import config 
-
 config.init()
-
 from config import numcolors, args
 global numcolors
 from mVAE import train, test, vae,  thecolorlabels, optimizer
 
-
-for epoch in range(1, 201):
-    
+#train for 200 eppochs
+for epoch in range(1, 201):  
     train(epoch,'iterated')
     colorlabels = np.random.randint(0,10,100000)#regenerate the list of color labels at the start of each test epoch
     numcolors = 0
     if epoch % 5 ==0:
-        test('all')  
-   
+        test('all')   
     if epoch in [1, 25,50,75,100,150,200,300,400,500]:
         checkpoint =  {
                  'state_dict': vae.state_dict(),
